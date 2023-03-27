259ff542db50ad7ac70489525e13769a0fe0d7c7
oleg-medovikov <oleg.medovikov@mail.ru>
2023-03-27 16:16:07 +0300

    add location html

--- App/views.py
+++ App/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+
 from App.models import Location
 
 
 def home(request):
     karta = Location.objects.all()
     return render(request, 'home.html', {"locations": karta})
+
+
+def location(requests):
+    pass
+
+
+def add_location(requests):
+    pass
+
+
+def edit_location(requests):
+    pass
+
+
+def delete_location(requests):
+    pass
--- arhmweb/urls.py
+++ arhmweb/urls.py
@@ -4,5 +4,17 @@ from App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+
+    # Path to ADD Karta
+    path('add_location', views.add_location, name='add_location'),
+    path('edit_location', views.edit_location, name='edit_location'),
+    # просмотр и удаление
+    path('location/<str:node_id>', views.location, name='location'),
+    path(
+        'delete_location/<str:node_id>',
+        views.delete_location,
+        name='delete_location'
+        ),
+
 ]
