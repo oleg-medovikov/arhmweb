4d4491574b7e576514ce1b7769f4d6f17d1c0f51
oleg-medovikov <oleg.medovikov@mail.ru>
2023-03-28 14:06:47 +0300

    add location is worked

--- App/views.py
+++ App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from json import loads
 
 from App.models import Location
 
@@ -14,12 +15,33 @@ def location(requests):
     pass
 
 
-def add_location(requests):
-    pass
+def add_location(request):
+    if request.method == "POST":
+        if request.POST.get('name_node') \
+                and request.POST.get('declension') \
+                and request.POST.get('contact_list_id') \
+                and request.POST.get('district') \
+                and request.POST.get('district_id'):
+            Loc = Location(
+                name_node=request.POST.get('name_node'),
+                declension=request.POST.get('declension'),
+                contact_list_id=loads(request.POST.get('contact_list_id')),
+                district=request.POST.get('district'),
+                district_id=request.POST.get('district_id'),
+                street=request.POST.get('street'),
+                dist=request.POST.get('dist'),
+                )
+            Loc.node_id = Location.objects.count()
+            Loc.street = True if request.POST.get('street') == 'on' else False
+            Loc.dist = True if request.POST.get('dist') == 'on' else False
+            Loc.save()
+            messages.success(request, "Локация добавлена!")
+            return HttpResponseRedirect("/")
 
 
-def edit_location(requests):
-    pass
+def edit_location(request):
+    if request.method == "POST":
+        print('test!')
 
 
 def delete_location(requests):
