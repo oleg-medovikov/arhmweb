6ff52298a77c5b5d990dff17f430753af95d6715
oleg-medovikov <oleg.medovikov@mail.ru>
2023-04-27 16:51:37 +0300

    add edit location

--- App/views.py
+++ App/views.py
@@ -7,7 +7,7 @@ from App.models import Location
 
 
 def karta(request):
-    karta = Location.objects.all()
+    karta = Location.objects.all().order_by('-date_update')
     return render(request, 'karta.html', {"locations": karta})
 
 
@@ -15,8 +15,10 @@ def home(request):
     return render(request, 'home.html')
 
 
-def location(requests):
-    pass
+def location(request, node_id):
+    location = Location.objects.get(node_id=node_id)
+    if location is not None:
+        return render(request, "edit_location.html", {'location': location})
 
 
 def add_location(request):
@@ -36,8 +38,8 @@ def add_location(request):
                 dist=request.POST.get('dist'),
                 )
             Loc.node_id = Location.objects.count()
-            Loc.street = True if request.POST.get('street') == 'on' else False
-            Loc.dist = True if request.POST.get('dist') == 'on' else False
+            Loc.street = True if Loc.street == 'True' else False
+            Loc.dist = True if Loc.dist == 'True' else False
             Loc.save()
             messages.success(request, "Локация добавлена!")
             return HttpResponseRedirect("karta")
@@ -45,7 +47,21 @@ def add_location(request):
 
 def edit_location(request):
     if request.method == "POST":
-        print('test!')
+        location = Location.objects.get(node_id=request.POST.get('node_id'))
+        if location is not None:
+            location.name_node = request.POST.get('name_node')
+            location.declension = request.POST.get('declension')
+
+            list_ = loads(request.POST.get('contact_list_id'))
+            location.contact_list_id = list_
+
+            location.district = request.POST.get('district')
+            location.district_id = request.POST.get('district_id')
+            location.street = request.POST.get('street')
+            location.dist = request.POST.get('dist')
+            location.save()
+            messages.success(request, "Локация обновлена!")
+            return HttpResponseRedirect("karta")
 
 
 def delete_location(requests):
